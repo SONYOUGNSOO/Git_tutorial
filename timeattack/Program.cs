fcaa4b8dea57c3313ab13c0be48de1b2c6d3f633
SONYOUNGSOO <cs003304@gmail.com>
2021-04-29 17:59:53 +0900

    초기 커밋.

---
+++ timeattack/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+namespace timeattack
+{
+    class Program
+    {
+        static void longtime()
+        {
+            Thread.Sleep(3000);
+        }
+        static void Main(string[] args)
+        {
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            longtime();
+            timer.Stop();
+
+            Console.WriteLine($"{timer.Elapsed.TotalMilliseconds}");
+            Console.WriteLine($"{timer.Elapsed.Seconds}");
+        }
+    }
+}
